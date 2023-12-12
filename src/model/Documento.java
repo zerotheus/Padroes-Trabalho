021f66d512e63fe8a57aa29d93fa9fcc43003bd5
ntheus <matheusOliveira.7313@gmail.com>
2023-12-11 22:54:40 -0300

    accept

--- src/model/Documento.java
+++ src/model/Documento.java
@@ -1,7 +1,10 @@
 package model;
 
+import visitor.Visitor;
+
 public interface Documento {
 	public String formatar();
 	public boolean validar();
 	public Integer pontuar();
+	public void accept(Visitor visitor);
 }
